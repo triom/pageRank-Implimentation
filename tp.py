e665cb45473d011bff7747c1cfeba49540d094f4
triomphantesonwa <triomphantesonwa@gmail.com>
2023-02-03 11:22:11 +0100

    create matrix

--- tp.py
+++ tp.py
@@ -1,10 +1,19 @@
-#pageRank implementation
-#propagation d'épidemie
+# pageRank implementation
+# propagation d'épidemie
 # on a 7115
 # taille de matrice 7115²
 
 def buildMatrix():
-    #extreaire le fichier
+    # init array
+    rows, cols = (7115, 7115)
+    transition_mtx = [[0]*cols]*rows
+    #créer un dict pour faire les liens
+    dict_link = {}
+    temp = []
+    #for row in transition_mtx:
+    #print(row)
+    
+    # extreaire le fichier
     with open('Wiki-Vote.txt') as f:
         i = 1
         while i:
@@ -13,8 +22,10 @@ def buildMatrix():
                 break
             link = line.strip()
             for node in link.split():
-                print(node)
-            i-=1
+                temp.append(node)
+                dict_link[temp[0]] = temp[1]
+            i -= 1
+            print(temp[1])
 
 
 buildMatrix()
